0af8bdc5fbf3f3188b9e2f541db978537930eb4e
Corne2Plum3 <57914482+Corne2Plum3@users.noreply.github.com>
2022-02-12 23:41:31 +0100

    Add FFMPEG detection and fix 'SystemExit' window

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from functools import partial
 import json
 import os
 import pathlib
+from pydub import AudioSegment
+import sys
 import tkinter as tk
 import tkinter.filedialog
 import tkinter.font as tkFont
@@ -15,6 +17,9 @@ import webbrowser
 
 # init
 try:
+    # ffmpeg detector init
+    ffmpeg_detect_file_test = "nothing2.ogg"  # used in detectFFMPEG(). Must be a ogg audio
+
     with open("config.json", "r") as file:  # all settings from config.json
         config_data = json.loads(file.read())  # parse it as dict
 
@@ -23,6 +28,7 @@ try:
         colors = config_data["colors"]  # dict with some colors used in the app
         delete_files_when_cancel = config_data["delete_files_when_cancel"]  # if the cancel button is pressed, delete generated files? (0 or 1)
         difficulties = config_data["init"]["difficulties"]   # the dict of lists of 2 elements {"diff_name": [map_mode (str), fnf_json_path]}
+        url_ffmpeg_tutorial = config_data["url_ffmpeg_tutorial"]
         url_github = config_data["url_github"]  # link to the application's GitHub
         url_help = config_data["url_help"]  # link to get the documentation
         verify_inputs = bool(int(config_data["verify_inputs"]))  # 0 or 1 in the JSON, False or True in this program
@@ -1034,6 +1040,110 @@ class New_difficulty_window:
 
             self.window.mainloop()
 
+class No_FFMPEG_window:
+    """
+        Class:
+            Object that represent a window which allow to create a difficulty.
+            Only 1 object of this class should be created. This object is a tk.Tk window. It doesn't have a master
+        Arguments:
+            Nothing.
+    """
+    def __init__(self):
+        self.__is_open = False
+
+    def openTutorial(self):
+        """
+            Class method:
+                Open the web browser to the tutorial to install ffmpeg.
+        """
+        global url_ffmpeg_tutorial
+        webbrowser.open(url_ffmpeg_tutorial)
+
+    def openWindow(self):
+        """
+            Class method:
+                Initialize and open the window.
+            Arguments:
+                None.
+            Return:
+                Nothing
+        """
+        global colors
+
+        if not(self.__is_open):
+            self.__is_open = True
+
+            # Create and init the window
+            self.window = tk.Tk()
+            self.window.title("FFMPEG isn't installed.")
+            self.window.geometry("320x200")
+            self.window.resizable(width=False, height=False)  # the window can't be resized
+            
+            # fonts (I know the code is a mess at this point...)
+            self.font_sans_10 = tkFont.Font(family="Barlow Condensed", size=10)
+            self.font_sans_10_b = tkFont.Font(family="Barlow Condensed", size=10, weight="bold")
+            self.font_sans_10_u = tkFont.Font(family="Barlow Condensed", size=10, underline=True)
+            self.font_sans_12 = tkFont.Font(family="Barlow Condensed", size=12)
+
+            # Widgets
+            frame_body = tk.Frame(self.window, padx=4, pady=4)  # used to create padding for the rest of the window
+            frame_body.grid(row=0, column=0, sticky="nswe")
+
+            # place widgets
+            header = tk.Label(frame_body, anchor="sw", justify="center", padx=4, pady=8, text='FFMPEG no found :(')
+            header.grid(row=0, column=0, columnspan=2, sticky="we")
+            header.config(font=self.font_sans_10_b)
+
+            body_message = tk.Message(frame_body,
+                anchor="nw",
+                justify="left",
+                padx=4,
+                width=600,
+                text="""FFMPEG isn't detected on your device, so the program will not work.\n
+                Please install FFMPEG then restart the program.
+                """
+            )
+            body_message.grid(row=1, column=0, columnspan=2, sticky="we")
+            body_message.config(font=self.font_sans_10)
+
+            button_close = tk.Button(frame_body, command=sys.exit, text="Close")  # ends the whole program
+            button_close.grid(row=2, column=0)
+            button_close.config(font=self.font_sans_10)
+
+            button_close = tk.Button(frame_body, command=self.openTutorial, text="How to install FFMPEG")  # opens page to the tutorials
+            button_close.grid(row=2, column=1)
+            button_close.config(font=self.font_sans_10)
+            
+            self.window.mainloop()
+
+def detectFFMPEG():
+    """ 
+        Method:
+            Try to detect FFMPEG by running a command that need to use it.
+            If it finishes like the issue #5, that means there's not FFMPEG installed. 
+        Argument:
+            None.
+        Return:
+            (bool): True if FFMPEG were detected, otherwise returns false.
+    """
+    """
+    try:
+        AudioSegment.from_file(ffmpeg_detect_file_test, format="ogg")
+    except:
+        return False
+    else:
+        return True
+    """
+
+    try:
+        AudioSegment.from_file(ffmpeg_detect_file_test, format="ogg")
+    except FileNotFoundError:
+        ffmpeg_detected = False
+    else:
+        ffmpeg_detected = True
+    
+    return ffmpeg_detected
+
 def fileExists(file_path):
     """
         Method:
@@ -1081,11 +1191,20 @@ def setFilePath(string_var):
     return selected_file
 
 # WHERE THE PROGRAM STARTS
+
 try:
+    if True or not(detectFFMPEG()):  # ffmpeg not detected
+        ffmpeg_window = No_FFMPEG_window()
+        ffmpeg_window.openWindow()
     
-    os.makedirs("output", exist_ok=True)  # create output folder if not exists
-    root = Main_window()  # app window
-    root.openWindow()
+    else:
+        os.makedirs("output", exist_ok=True)  # create output folder if not exists
+        root = Main_window()  # app window
+        root.openWindow()
+
+except SystemExit:  # don't call error message when SystemExit is raised
+    pass
+
 except:
     error_window = Crash_window(traceback.format_exc())
     error_window.openWindow()
